f1499c30cbf944466536936d173f44d4dff9b2ec
Roman Gherta <rgherta@users.noreply.github.com>
2016-09-05 01:16:12 +0300

    Update W2 - LinearRegression.py

--- W2 - LinearRegression.py
+++ W2 - LinearRegression.py
@@ -28,3 +28,24 @@ print "Variance score ", lr.score(test['sqft_living'].reshape(-1,1), test['price
 matplotlib.pyplot.plot(test['sqft_living'].reshape(-1,1), test['price'].reshape(-1,1),".",
                        test['sqft_living'].reshape(-1,1), lr.predict(test['sqft_living'].reshape(-1,1)), '-')
 
+#Creating Model Based On Features
+features = ['bedrooms', 'bathrooms', 'sqft_living', 'sqft_lot', 'floors', 'zipcode']
+lrm = sklearn.linear_model.LinearRegression()
+lrm.fit(train[features], train['price'])
+
+print "The coefficients are ", lrm.coef_
+print "Mean Square Error ", numpy.mean((lrm.predict(test[features]) - test['price'])**2)
+print "RMSE ", math.sqrt(numpy.mean((lrm.predict(test[features]) - test['price'])**2))
+print "Variance score ", lrm.score(test[features], test['price'])
+
+#Comparing House1
+house1 = csv[csv.id == 5309101200]
+print house1
+print "Features Model ", lrm.predict(house1[features])
+print "Sqft Model ", lr.predict(house1['sqft_living'].reshape(-1,1))
+
+#Comparing House2
+house2 = csv[csv.id == 1925069082]
+print house2
+print "Features Model ", lrm.predict(house2[features])
+print "Sqft Model ", lr.predict(house2['sqft_living'].reshape(-1,1))
